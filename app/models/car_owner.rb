565ca4c11cda6b5b4a76c2f71e06897ad9fba568
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-06-06 19:40:58 -0700

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,32 @@
 class CarOwner
 
+  @@all = []
   attr_reader :name
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+    
+  # - `CarOwner.all` Get a list of all owners
+  def self.all
+    @@all
+  end
 
+  # - `CarOwner#cars` Get a list of all the cars that a specific owner has
+  def cars
+    Car.all.select {|owner| owner.car_owner == self}
+  end
+
+  # - `CarOwner#mechanics` Get a list of all the mechanics that a specific owner goes to
+  def mechanics
+    cars.map {|car| car.mechanic}
+  end
+
+  # - `CarOwner.average_number_of_cars` Get the average amount of cars owned for all owners
+    def self.average_number_of_cars
+      total = Car.all.map {|car| car.car_owner}.size
+      total.to_f / CarOwner.all.size
+    end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,18 @@ def reload
   load 'config/environment.rb'
 end
 
+sungwoo = CarOwner.new("Sungwoo")
+jared = CarOwner.new("Jared")
+malcom = CarOwner.new("Malcom")
+thomas = Mechanic.new("Thomas", "Truck")
+mark = Mechanic.new("Mark", "Truck")
+bogdan = Mechanic.new("Bogdan", "Sedan")
+
+Ford_1997 = Car.new("Ford", "F1", "Truck", sungwoo, thomas)
+BMW_1997 = Car.new("BMW", "M3", "Muslce", sungwoo, mark)
+Chevrolet_1997 = Car.new("Cheavy", "Truck", "Sports",jared, mark)
+Honda_1997 = Car.new("Honda", "Civic", "Truck", malcom, bogdan)
+
+
+
 binding.pry

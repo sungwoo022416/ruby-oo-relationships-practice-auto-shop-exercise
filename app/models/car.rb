8e14ab5f16cb3bc6f463b5f3716aa862bf225c2c
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-06-06 19:40:55 -0700

    commit tree 9409f65593f17b7f64d776526488da0fadfe06e1

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,31 @@
 class Car
+  @@all = []
+  
+  attr_accessor :car_owner, :mechanic
+  attr_reader :make, :model, :classification
 
-  attr_reader :make, :model
-
-  def initialize(make, model, classification)
+  def initialize(make, model, classification, car_owner, mechanic)
     @make = make
     @model = model
     @classification = classification
+    @car_owner = car_owner
+    @mechanic = mechanic
+
+    @@all <<self
+  end
+
+    # - `Car.all` Get a list of all cars
+  def self.all
+    @@all
+  end
+
+  # - `Car.classifications` Get a list of all car classifications
+  def self.classifications
+    Car.all.select {|car| car.classification}.map(&:classification)
+  end
+  # - `Car.find_mechanics(classification)` Get a list of mechanics that have an expertise that matches the passed in car classification
+  def self.find_mechanic(classification)
+    Mechanic.all.select {|mech| mech.specialty = classification}.map(&:name)
   end
 
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,32 @@
 class Mechanic
 
+  @@all = []
   attr_reader :name, :specialty
 
 def initialize(name, specialty)
   @name = name
   @specialty = specialty
+
+  @@all << self
+end
+def specialty=(specialty)
+  @specialty = specialty
+end
+# - `Mechanic.all` Get a list of all mechanics
+def self.all
+  @@all
 end
 
-
+# - `Mechanic#cars` Get a list of all cars that a mechanic services
+def cars
+  Car.all.select {|car| car.mechanic == self}
+end
+# - `Mechanic#car_owners` Get a list of all the car owners that go to a specific mechanic
+def car_owners
+  cars.map(&:car_owner)
+end
+# - `Mechanic#car_owners_names` Get a list of the names of all car owners who go to a specific mechanic
+def car_owners_names
+  car_owners.map(&:name)
+end
 end
